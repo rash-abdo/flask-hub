7a117f7b2565308f25ec35b9316481b865657f91
abdo <abdddo@gmail.com>
2025-03-26 01:19:17 +0200

    added upload folder,create blog function

--- project/app.py
+++ project/app.py
@@ -13,6 +13,10 @@ def create_app():
     app = Flask(__name__, template_folder='templates')
     app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
+    app.config['UPLOAD_FOLDER'] = 'project/uploads'
+    
+    if not os.path.exists('project/uploads'):
+        os.makedirs('project/uploads')
 
     db.init_app(app)
     bcrypt.init_app(app)
--- project/blueprints/blogs/controllers.py
+++ project/blueprints/blogs/controllers.py
@@ -1,7 +1,20 @@
 from flask import (render_template,request,
-                   redirect,url_for,flash)
+                   redirect,url_for,flash,session)
+from project.app import db
+from project.blueprints.blogs.models import Blogs
+import os
+import datetime
 
 
 def create_blog():
     if request.method == 'GET':
         return render_template('myblogs.html')
+    if request.method == 'POST':
+        title = request.form.get('title')
+        blog = request.form.get('blog')
+        if not os.path.exists(f'project/uploads/blogs/Uid_{session["uid"]}'):
+            os.makedirs(f'project/uploads/blogs/Uid_{session["uid"]}')
+        with open(f'project/uploads/blogs/Uid_{session["uid"]}/{title}.txt',"w") as f:
+            f.write(blog)
+        
+        return redirect(url_for('blogs.create_blog'))
