c50bad84ddc9f38fa35f86605bc8b5e4cadff56e
abdo <abdddo@gmail.com>
2025-03-29 18:41:31 +0200

    added show other profiles func

--- project/blueprints/blogs/routes.py
+++ project/blueprints/blogs/routes.py
@@ -1,9 +1,13 @@
-from project.blueprints.blogs.controllers import (myblogs,edit_blog,home)
+from project.blueprints.blogs.controllers import (delete_blog,create_blog,
+                                                  view_myblogs,edit_blog,home,other_profile)
 from flask import Blueprint
 
 
 blogs = Blueprint('blogs',__name__,template_folder='templates')
 
 blogs.add_url_rule('/home', 'home', home, methods=['GET'])
-blogs.add_url_rule('/myblogs','myblogs',myblogs,methods=['GET','POST'])
+blogs.add_url_rule('/myblogs','view_myblogs',view_myblogs,methods=['GET','POST'])
+blogs.add_url_rule('/delete blog','delete_blog',delete_blog,methods=['GET','POST'])
+blogs.add_url_rule('/create blog','create_blog',create_blog,methods=['GET','POST'])
 blogs.add_url_rule('/edit blog','edit_blog',edit_blog,methods=['GET','POST'])
+blogs.add_url_rule('/<users_id>','other_profile',other_profile,methods=['GET','POST'])
